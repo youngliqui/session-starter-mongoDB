ac2da178a8478d1a65db6b70f9d80f1b3af5ef7e
youngliqui <super.pav-79@yandex.by>
2024-10-15 09:05:01 +0300

    spring init

---
+++ src/main/java/ru/clevertec/session_starter/SessionStarterApplication.java
@@ -0,0 +1,13 @@
+package ru.clevertec.session_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/clevertec/session_starter/SessionStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.clevertec.session_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
