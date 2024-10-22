bb34d5939caa39e5f5d240842df549ace21b75a2
youngliqui <super.pav-79@yandex.by>
2024-10-22 18:43:28 +0300

    refactor: Changed the logic of getting properties from application.yaml

--- src/main/java/ru/clevertec/session_starter/config/SchedulerConfig.java
+++ src/main/java/ru/clevertec/session_starter/config/SchedulerConfig.java
@@ -1,11 +1,18 @@
 package ru.clevertec.session_starter.config;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Slf4j
+@Component
 @EnableScheduling
 @ConditionalOnProperty(name = "scheduler.enabled", havingValue = "true", matchIfMissing = true)
 public class SchedulerConfig {
+    @PostConstruct
+    public void init() {
+        log.info("SchedulerConfig has been initialized and is active.");
+    }
 }
